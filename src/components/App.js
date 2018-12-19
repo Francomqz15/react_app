62e2750072c9812a1f19a393a6d16f5d34b1edb0
Franco Marquez <fmarquez@deloitte.com>
2018-12-19 13:46:38 -0600

    add redux to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Navbar, Grid, Row, Col } from 'react-bootstrap';
+import { Provider } from 'react-redux';
+import store from '../store';
 //Compontens
 import Header from './Core/Header/header';
 import ProductList from './ProductList';
@@ -8,23 +10,25 @@ import ShoppingCart from './ShoppingCart';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      	<Header></Header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Grid>
-          <Row>
-            <Col sm={8}>
-              <ProductList />
-            </Col>
-            <Col sm={4}>
-              <ShoppingCart />
-            </Col>
-          </Row>
-        </Grid>
-      </div>
-    );
+      <Provider store={store}>
+            <div className="App">
+                  <Header></Header>
+                  <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                  </p>
+                  <Grid>
+                    <Row>
+                       <Col sm={8}>
+                          <ProductList />
+                        </Col>
+                        <Col sm={4}>
+                            <ShoppingCart />
+                        </Col>
+                      </Row>
+                  </Grid>
+            </div>
+      </Provider>
+      );
   }
 }
 
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -24,8 +24,15 @@ class ProductList extends Component {
 				{ id: 1, name: "Hipster Ultimate", price: 299, image: "https://s3.amazonaws.com/makeitreal/projects/e-commerce/camiseta-1.jpg" },
 				{ id: 2, name: "On Motion Live", price: 99, image: "https://s3.amazonaws.com/makeitreal/projects/e-commerce/camiseta-2.jpg" },
 				{ id: 3, name: "Underground Max", price: 149, image: "https://s3.amazonaws.com/makeitreal/projects/e-commerce/camiseta-3.jpg" }
-			]
+			],
+			produxtSelected: {}
 		}
+
+		this.onChange =  this.onChange.bind(this);
+	}
+
+	onChange(e) {
+		this.setState({ [e.target.name]: e.target.value });
 	}
 
 	render() {
@@ -47,7 +54,7 @@ class ProductList extends Component {
 	}
 
 	addToCart(product) {
-
+		this.setState({produxtSelected: product});
   	}
 }
 
