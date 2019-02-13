fc8c222933c844415cf09623f857270da031d201
Franco Marquez <fmarquez@deloitte.com>
2019-02-13 12:51:45 -0600

    set state to extend weather

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import Header from './Core/Header/header';
 import ProductList from './ProductList';
 import ShoppingCart from './ShoppingCart';
 import LocationList from './LocationList';
+import ForecastExtended from './ForecastExtended.js';
 import './App.css';
 
 const cities = [
@@ -19,27 +20,37 @@ const cities = [
 
 class App extends Component {
 
+  constructor() {
+    super();
+    this.state = {city: null};
+  }
 
   handleSelectedLocation =  city =>  {
+    this.setState({city})
     console.log("handldeWeatherLocationClick")
   }
 
   render() {
+    const {city}  = this.state;
+
     return (
  
-
       <Provider store={store}>
           <div className="container-fluid">
                 <Header></Header>
                     <Row>
-                        Titulo
+                        Weather App
                     </Row>
                     <Row>
                          <Col xs={12} md={6}>
                              <LocationList cities={cities} onSelectedLocation={this.handleSelectedLocation} ></LocationList>
                          </Col>
                          <Col xs={12} md={6}>
-                             <div className="details"></div>
+                             <div className="details">
+                                { 
+                                  !city ? null: <ForecastExtended city={city}> </ForecastExtended>
+                                }
+                             </div>
                          </Col>
                     </Row>  
                    
