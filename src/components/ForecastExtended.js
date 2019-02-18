c6a3261e139bde229110551fa9619ff218f6bc98
Franco Marquez <fmarquez@deloitte.com>
2019-02-18 11:29:13 -0600

    add foreCastItem

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,11 +1,30 @@
 import React , { Component} from 'react';
 import PropTypes from 'prop-types';
+import ForecastItem from './Forecastitem';
 import './styles.css';
 
+
+const days = ['Lunes', 'Martes','Miercoles','Jueves','Viernes'];
+
+const data = {
+	temperature: 10,
+	humidity: 20,
+	weatherState: 'normal',
+	wind: 'normal'
+};
+
 class ForecastExtended extends Component {
+
+	renderForecastItemDays() {
+		return days.map(day => (<ForecastItem weekDay={day} hour={10} data={data}></ForecastItem>))
+	}
+
 	render() {
 		const {city} = this.props;
-		return (<div> <h2 className="forecast-title"> Pronostico Extendido para {city} </h2> </div>);
+		return (<div> 
+					<h2 className="forecast-title"> Pronostico Extendido para {city} </h2> 
+					{this.renderForecastItemDays()}
+				</div>);
 	}
 }
 
